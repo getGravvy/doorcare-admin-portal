d870a7e67f224c33f23db04f175336340a5ba687
getGravvy <getgravvy42@gmail.com>
2025-04-27 11:02:21 -0600

    Update App.js

--- doorcare-admin-portal/App.js
+++ doorcare-admin-portal/App.js
@@ -1,1 +1,19 @@
-console.log('DoorCare Admin Portal');
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminLoginPage from './pages/AdminLoginPage';
+import AdminDashboardPage from './pages/AdminDashboardPage';
+import ManageBookingsPage from './pages/ManageBookingsPage';
+import CompanyProfilePage from './pages/CompanyProfilePage';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdminLoginPage />} />
+        <Route path="/dashboard" element={<AdminDashboardPage />} />
+        <Route path="/bookings" element={<ManageBookingsPage />} />
+        <Route path="/profile" element={<CompanyProfilePage />} />
+      </Routes>
+    </Router>
+  );
+}
